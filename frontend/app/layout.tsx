436638a7cc211c86ce17712be47fdc6b14fe035e
Markus Evanger <markus@kult.design>
2025-12-23 10:15:22 +0100

    Remove template content

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -8,9 +8,6 @@ import {VisualEditing, toPlainText} from 'next-sanity'
 import {Toaster} from 'sonner'
 
 import DraftModeToast from '@/app/components/DraftModeToast'
-import Footer from '@/app/components/Footer'
-import Header from '@/app/components/Header'
-import * as demo from '@/sanity/lib/demo'
 import {sanityFetch, SanityLive} from '@/sanity/lib/live'
 import {settingsQuery} from '@/sanity/lib/queries'
 import {resolveOpenGraphImage} from '@/sanity/lib/utils'
@@ -26,8 +23,8 @@ export async function generateMetadata(): Promise<Metadata> {
     // Metadata should never contain stega
     stega: false,
   })
-  const title = settings?.title || demo.title
-  const description = settings?.description || demo.description
+  const title = settings?.title || 'Site'
+  const description = settings?.description
 
   const ogImage = resolveOpenGraphImage(settings?.ogImage)
   let metadataBase: URL | undefined = undefined
@@ -44,7 +41,7 @@ export async function generateMetadata(): Promise<Metadata> {
       template: `%s | ${title}`,
       default: title,
     },
-    description: toPlainText(description),
+    description: description ? toPlainText(description) : undefined,
     openGraph: {
       images: ogImage ? [ogImage] : [],
     },
@@ -63,22 +60,18 @@ export default async function RootLayout({children}: {children: React.ReactNode}
   return (
     <html lang="en" className={`${inter.variable} bg-white text-black`}>
       <body>
-        <section className="min-h-screen pt-24">
-          {/* The <Toaster> component is responsible for rendering toast notifications used in /app/client-utils.ts and /app/components/DraftModeToast.tsx */}
-          <Toaster />
-          {isDraftMode && (
-            <>
-              <DraftModeToast />
-              {/*  Enable Visual Editing, only to be rendered when Draft Mode is enabled */}
-              <VisualEditing />
-            </>
-          )}
-          {/* The <SanityLive> component is responsible for making all sanityFetch calls in your application live, so should always be rendered. */}
-          <SanityLive onError={handleError} />
-          <Header />
-          <main className="">{children}</main>
-          <Footer />
-        </section>
+        {/* The <Toaster> component is responsible for rendering toast notifications used in /app/client-utils.ts and /app/components/DraftModeToast.tsx */}
+        <Toaster />
+        {isDraftMode && (
+          <>
+            <DraftModeToast />
+            {/*  Enable Visual Editing, only to be rendered when Draft Mode is enabled */}
+            <VisualEditing />
+          </>
+        )}
+        {/* The <SanityLive> component is responsible for making all sanityFetch calls in your application live, so should always be rendered. */}
+        <SanityLive onError={handleError} />
+        <main>{children}</main>
         <SpeedInsights />
       </body>
     </html>
--- studio/src/schemaTypes/index.ts
+++ studio/src/schemaTypes/index.ts
@@ -1,10 +1,4 @@
-import {person} from './documents/person'
-import {page} from './documents/page'
-import {post} from './documents/post'
-import {callToAction} from './objects/callToAction'
-import {infoSection} from './objects/infoSection'
 import {settings} from './singletons/settings'
-import {link} from './objects/link'
 import {blockContent} from './objects/blockContent'
 
 // Export an array of all the schema types.  This is used in the Sanity Studio configuration. https://www.sanity.io/docs/schema-types
@@ -12,13 +6,6 @@ import {blockContent} from './objects/blockContent'
 export const schemaTypes = [
   // Singletons
   settings,
-  // Documents
-  page,
-  post,
-  person,
   // Objects
   blockContent,
-  infoSection,
-  callToAction,
-  link,
 ]
